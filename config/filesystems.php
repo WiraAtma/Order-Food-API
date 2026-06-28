3d712bd0b75d38c1ab21125968f6849efb1221b0
Wira Atmaja <atmajawira1908@gmail.com>
2026-06-28 13:46:57 +0800

    add

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,7 +59,16 @@ return [
             'throw' => false,
             'report' => false,
         ],
-
+        'supabase' => [
+            'driver' => 's3',
+            'key' => env('SUPABASE_KEY'),
+            'secret' => env('SUPABASE_SECRET'),
+            'region' => env('SUPABASE_REGION', 'ap-southeast-1'),
+            'bucket' => env('SUPABASE_BUCKET'),
+            'endpoint' => env('SUPABASE_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+            'url' => env('SUPABASE_PUBLIC_URL'),
+        ],
     ],
 
     /*
